2b34e5725e739273ae8add52eee8ddde3b06abf5
Anton Ryabyh <lenant255@gmail.com>
2021-03-02 14:59:11 +0500

    удален комментарий

--- app/src/main/java/com/example/android/kotlinone/MainActivity.kt
+++ app/src/main/java/com/example/android/kotlinone/MainActivity.kt
@@ -56,7 +56,6 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
- //   lkklkl
     override fun onSaveInstanceState(savedInstanceState: Bundle) {
         super.onSaveInstanceState(savedInstanceState)
         savedInstanceState.putInt(STATE_NUMBER, x + 1);
